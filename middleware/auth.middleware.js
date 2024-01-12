378134e0d7c51d511929345443334e4ba50b9869
ak8459 <87300147+ak8459@users.noreply.github.com>
2024-01-12 13:46:55 +0530

    updated

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -8,8 +8,6 @@ const auth = async (req, res, next) => {
         // console.log(token);
         if (token) {
             let user = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            // const User = await userModel.findOne({ email: user.email, 'tokens.token': token });
-            // const User = await userModel.findOne({ email: user.user.email, });
             console.log(user);
             req.body.userId = user.user._id
             req.user = user.user;
